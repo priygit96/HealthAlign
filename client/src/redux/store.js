a65f2b4ce0b1a93ee044be519aa11fc43ab04cd6
Priyanshu Gupta <pgupta962000@gmail.com>
2023-08-14 23:56:17 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { alertSlice } from "./features/alertSlice";
 import { userSlice } from "./features/userSlice";
-
+import { alertSlice } from "./features/alertSlice";
 export default configureStore({
   reducer: {
     alerts: alertSlice.reducer,

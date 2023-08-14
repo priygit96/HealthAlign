9bc872f18c5c739780a551cf44c024e796863d04
Priyanshu Gupta <pgupta962000@gmail.com>
2023-08-14 23:48:52 +0530

    Update Users.js

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Layout from "./../../components/Layout";
-import axios from "axios";
 import { Table } from "antd";
+import axios from "axios";
+import Layout from "./../../components/Layout";
 const Users = () => {
   const [users, setUsers] = useState([]);
 
@@ -54,7 +54,7 @@ const Users = () => {
   return (
     <Layout>
       <h1 className="text-center m-2">Users List</h1>
-      <Table columns={columns} dataSource={users} />
+        <Table columns={columns} dataSource={users} />
     </Layout>
   );
 };
